64abf1d35d82d79c265e76ed0c4542602357424d
purna99 <cpurna1@gmail.com>
2017-09-19 18:25:23 -0500

    created statuses component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FormsModule } from '@angular/forms';
+import { StatusesService } from './statuses/statuses.service';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { StatusesComponent } from './statuses/statuses.component';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyAQ0o6x-7Qwu_FQeqqjhx6U8HVpQQ4ao0M",
@@ -16,32 +19,20 @@ export const firebaseConfig = {
   messagingSenderId: "381281780656"
 };
 
-// <script src="https://www.gstatic.com/firebasejs/4.3.1/firebase.js"></script>
-// <script>
-//   // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyAQ0o6x-7Qwu_FQeqqjhx6U8HVpQQ4ao0M",
-//     authDomain: "statusupdateapp-f97a9.firebaseapp.com",
-//     databaseURL: "https://statusupdateapp-f97a9.firebaseio.com",
-//     projectId: "statusupdateapp-f97a9",
-//     storageBucket: "",
-//     messagingSenderId: "381281780656"
-//   };
-//   firebase.initializeApp(config);
-// </script>
-
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    SidebarComponent
+    SidebarComponent,
+    StatusesComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [StatusesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
